e927096b4482dde0f6e67ca736f5357595e45316
endqh306 <endqh306@gmail.com>
2023-04-02 01:45:49 +0900

    주석 첨가

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,20 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// ctrl+shift+L 단축키 전체 목록보기
+		// ctrl+ +, - 폰트 크기 증가/감소
+		// ctrl+D 한 줄 삭제
+		// ctrl+alt+shift+down 행단위 복사
+		// alt+shift+A 멀티컬럼 편집
+		// alt+up, down 행단위 이동
+		// ctrl+i 자동 들여쓰기
+		// ctrl+/  주석(토글) -> 한줄 주석
+		// ctrl+space 자동완성
+		System.out.println();
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 		System.out.println("Hello");
 	}
 
-}
+}    
